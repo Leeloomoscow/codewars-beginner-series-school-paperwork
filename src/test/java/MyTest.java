75a3b75c33093e39197472afb4d93909063d22db
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-14 12:51:09 +0300

    Codewark Beginner Series #1 School Paperwork

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,29 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
-
+    public void test1(){
+        assertEquals("Failed at paperWork(5,5)",25, Paper.paperWork(5,5));
     }
-}
+
+    private void assertEquals(String s, int i, int paperWork) {
+    }
+
+    private static class Paper {
+        public static int paperWork(int n, int m) {
+            {
+                int ret = 0;
+                if (n < 0 || m < 0){
+                    return 0;
+                } else {
+                    ret = n * m ;
+                }
+
+                return ret;
+            }
+        }
+        }
+    }
+
